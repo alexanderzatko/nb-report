30df077f9677aaf8428f355dcc790056cafcd9da
AZ <3430677+alexanderzatko@users.noreply.github.com>
2024-10-16 11:47:20 +0200

    Create i18n.js

---
+++ i18n.js
@@ -0,0 +1,23 @@
+import i18next from 'i18next';
+import HttpBackend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18next
+  .use(HttpBackend)
+  .use(LanguageDetector)
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json'
+    },
+    detection: {
+      order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
+      lookupQuerystring: 'lng',
+      lookupCookie: 'i18next',
+      lookupLocalStorage: 'i18nextLng',
+      caches: ['localStorage', 'cookie'],
+    }
+  });
+
+export default i18next;
