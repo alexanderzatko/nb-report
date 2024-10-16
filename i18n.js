80a38f76feb6c8c7332caec3e17daed4e60be908
AZ <3430677+alexanderzatko@users.noreply.github.com>
2024-10-16 19:31:21 +0200

    using the ES module version

--- i18n.js
+++ i18n.js
@@ -1,6 +1,6 @@
 import i18next from 'i18next';
-import HttpBackend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpBackend from 'i18next-http-backend/esm/index.js';
+import LanguageDetector from 'i18next-browser-languagedetector/esm/index.js';
 
 i18next
   .use(HttpBackend)
