293aed588286a3ef1f9d01cdf1ee597fff4b470c
AZ <3430677+alexanderzatko@users.noreply.github.com>
2024-10-17 08:23:28 +0200

    refector

--- public/i18n.js
+++ public/i18n.js
@@ -2,29 +2,36 @@ import i18next from '/node_modules/i18next/dist/esm/i18next.js';
 import HttpBackend from '/node_modules/i18next-http-backend/esm/index.js';
 import LanguageDetector from '/node_modules/i18next-browser-languagedetector/dist/esm/i18nextBrowserLanguageDetector.js';
 
-i18next
-  .use(HttpBackend)
-  .use(LanguageDetector)
-  .init({
-    fallbackLng: 'en',
-    load: 'languageOnly',
-    languageMapping: {
-      'en': 'en',
-      'en-GB': 'en',
-      'en-US': 'en'
-    },
-    debug: true,
-    returnObjects: true,
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json'
-    },
-    detection: {
-      order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
-      lookupQuerystring: 'lng',
-      lookupCookie: 'i18next',
-      lookupLocalStorage: 'i18nextLng',
-      caches: ['localStorage', 'cookie'],
-    }
-  });
+let initPromise = null;
 
-export default i18next;
+const initI18next = () => {
+  if (!initPromise) {
+    initPromise = i18next
+      .use(HttpBackend)
+      .use(LanguageDetector)
+      .init({
+        fallbackLng: 'en',
+        load: 'languageOnly',
+        languageMapping: {
+          'en': 'en',
+          'en-GB': 'en',
+          'en-US': 'en'
+        },
+        debug: true,
+        returnObjects: true,
+        backend: {
+          loadPath: '/locales/{{lng}}/{{ns}}.json'
+        },
+        detection: {
+          order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
+          lookupQuerystring: 'lng',
+          lookupCookie: 'i18next',
+          lookupLocalStorage: 'i18nextLng',
+          caches: ['localStorage', 'cookie'],
+        }
+      });
+  }
+  return initPromise;
+};
+
+export { i18next, initI18next };
