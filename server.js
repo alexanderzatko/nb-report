2ba1384b19cbe184bbf66cfb5e8e76efe579f2f5
AZ <3430677+alexanderzatko@users.noreply.github.com>
2024-10-21 09:50:51 +0200

    removed the not working forced reauth code

--- server.js
+++ server.js
@@ -220,11 +220,6 @@ app.post('/api/initiate-oauth', (req, res) => {
   const { state, scopes, forceReauth } = req.body;
   let authUrl = `${OAUTH_PROVIDER_URL}/oauth2/authorize?client_id=${OAUTH_CLIENT_ID}&redirect_uri=${OAUTH_REDIRECT_URI}&response_type=code&state=${state}&scope=${encodeURIComponent(scopes || '')}`;
   
-  // Add force re-authentication parameter if requested
-  if (forceReauth) {
-    authUrl += '&prompt=login&max_auth_age=0';
-  }
-  
   res.json({ authUrl });
 });
 
