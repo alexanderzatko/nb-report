b9557417fd83a0c061bb97dc2ee538e75296d8ef
AZ <3430677+alexanderzatko@users.noreply.github.com>
2024-10-25 13:28:21 +0200

    Update LocationManager.js

--- public/js/location/LocationManager.js
+++ public/js/location/LocationManager.js
@@ -5,7 +5,7 @@ import i18next from '/node_modules/i18next/dist/esm/i18next.js';
 class LocationManager {
   static instance = null;
 
-  constructor(i18next) {
+  constructor() {
     if (LocationManager.instance) {
       return LocationManager.instance;
     }
