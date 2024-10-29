6b47dd1c3f8895c8d42a0b23fa6e14c70058a6bf
AZ <3430677+alexanderzatko@users.noreply.github.com>
2024-10-29 07:23:21 +0100

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v99';  // Should match ConfigManager.js version
+const CACHE_VERSION = 'v100';  // Should match ConfigManager.js version
 const CACHE_NAME = 'snow-report-cache';
 const FULL_CACHE_NAME = `${CACHE_NAME}-${CACHE_VERSION}`;
 
