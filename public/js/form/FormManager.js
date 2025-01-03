bc27dd50f6cceec7cf3bf5cd966f11706eba74c1
AZ <3430677+alexanderzatko@users.noreply.github.com>
2025-01-03 19:15:02 +0100

    changed icons for trail status

--- public/js/form/FormManager.js
+++ public/js/form/FormManager.js
@@ -766,8 +766,8 @@ class FormManager {
       { value: '1', label: '★★★', title: this.i18next.t('form.trackConditions.1') },
       { value: '2', label: '★★', title: this.i18next.t('form.trackConditions.2') },
       { value: '3', label: '★', title: this.i18next.t('form.trackConditions.3') },
-      { value: '4', label: '∥', title: this.i18next.t('form.trackConditions.4') },
-      { value: '5', label: 'x', title: this.i18next.t('form.trackConditions.5') }
+      { value: '4', label: '≠', title: this.i18next.t('form.trackConditions.4') },
+      { value: '5', label: '=::=', title: this.i18next.t('form.trackConditions.5') }
     ];
   
     conditions.forEach(condition => {
