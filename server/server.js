ec49b6aabd9f948b6dbcd75069b716a1469ac242
AZ <3430677+alexanderzatko@users.noreply.github.com>
2025-01-19 19:00:36 +0100

    fixed path

--- server/server.js
+++ server/server.js
@@ -732,7 +732,7 @@ app.get('/api/check-session', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.listen(port, '0.0.0.0', () => {
