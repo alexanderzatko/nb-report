cb077ebf699dd42c6958873e170fe598d772c562
AZ <3430677+alexanderzatko@users.noreply.github.com>
2025-03-02 09:27:53 +0100

    Update ConfigManager.js

--- public/js/config/ConfigManager.js
+++ public/js/config/ConfigManager.js
@@ -13,7 +13,7 @@ class ConfigManager {
     this.logger = Logger.getInstance();
     this.config = {
       cache: {
-        version: 'v631', // update this for new app release and do the same in service-worker.js
+        version: 'v632', // update this for new app release and do the same in service-worker.js
         name: 'snow-report-cache',
         staticResources: [
           '/',
