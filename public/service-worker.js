dbf0890b7a11f1f02ae490f82279e6acb54fefe2
Alexander Zatko <alexander.zatko@nabezky.sk>
2025-11-27 06:32:56 +0100

    upping the version

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v663';  // Should match ConfigManager.js version
+const CACHE_VERSION = 'v664';  // Should match ConfigManager.js version
 const CACHE_NAME = 'snow-report-cache';
 const FULL_CACHE_NAME = `${CACHE_NAME}-${CACHE_VERSION}`;
 const OFFLINE_PAGE = '/offline.html';
